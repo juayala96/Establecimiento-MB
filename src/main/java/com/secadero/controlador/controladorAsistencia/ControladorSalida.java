c20d8cfd3fd11e3875e84f40f8d812b726d9530f
MichaelBentos <michaelbentos9@gmail.com>
2023-02-09 17:31:36 -0300

    09/02/2023 Michael

--- src/main/java/com/secadero/controlador/controladorAsistencia/ControladorSalida.java
+++ src/main/java/com/secadero/controlador/controladorAsistencia/ControladorSalida.java
@@ -3,9 +3,7 @@ package com.secadero.controlador.controladorAsistencia;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Tab;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
@@ -19,18 +17,39 @@ public class ControladorSalida {
     @FXML
     private Button btnSalir;
     @FXML
+    private Label labErrorCodigo;
+    @FXML
+    private Label labInformacion;
+    @FXML
     private Tab tabRegistroSalidaCodigo;
     @FXML
     private TextField textCodigo;
 
     @FXML
     void aceptar() {
+        if(!textCodigo.getText().equals("")){
+            if(validarNumeros(textCodigo.getText())){
+                labErrorCodigo.setText("");
 
+            } else {
+                labErrorCodigo.setText("Solo se permite Números");
+            }
+        } else {
+            Alert alerta = new Alert(Alert.AlertType.WARNING);
+            alerta.setTitle("Error!");
+            alerta.setContentText("Debe de completar el campo");
+            alerta.showAndWait();
+        }
     }
 
     @FXML
     void salir() throws IOException {
         closeWindowsPrincipal();
+    }
+
+    //---------------------------------------- Validación de Caracteres ------------------------------------------
+    public static boolean validarNumeros(String datos){
+        return datos.matches("[0-9]*");
     }
 
     // ---------------------------------- Cerrar Ventana -------------------------------------------
