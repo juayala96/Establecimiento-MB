cc3a681e7bc069ed75adf3ffc8f5e6ffe835d239
MichaelBentos <michaelbentos9@gmail.com>
2023-05-29 16:27:34 -0300

    29/05/2023 Michael Pre-Recibo e Informe Modificado

--- src/main/java/com/secadero/Secadero.java
+++ src/main/java/com/secadero/Secadero.java
@@ -11,11 +11,11 @@ import java.io.IOException;
 public class Secadero extends Application {
     public static void main(String[] args) {
         launch(args);
-    }
+    } ///
 
     @Override
     public void start(Stage primaryStage) throws IOException {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/vistas/ausencias.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/vistas/principal.fxml"));
         AnchorPane root = loader.load();
         Scene escena = new Scene(root);
         primaryStage.setScene(escena);
